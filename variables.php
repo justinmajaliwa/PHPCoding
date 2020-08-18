939a27889950ceb94e8c491cadbb4bb5204d260d
Justin John <majaliwa149@gmail.com>
2020-08-18 19:06:19 +0300

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,12 @@
+<?php
+//we start declaring variables
+
+   $name = "Kahema";
+   $act = 'My $name Loves PHP!';
+   
+   //Let see how fun programming is
+   
+   print($act);
+   print "<br>";
+   
+?>
